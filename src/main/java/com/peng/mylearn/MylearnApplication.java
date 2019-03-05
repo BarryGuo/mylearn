099f3f286633eca3450467d784f314020ef2fc1e
PENG <306674765@qq.com>
2019-03-05 23:28:58 +0800

    第一章：修改配置文件

--- src/main/java/com/peng/mylearn/MylearnApplication.java
+++ src/main/java/com/peng/mylearn/MylearnApplication.java
@@ -1,11 +1,19 @@
 package com.peng.mylearn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 /**
  * 纪念我的第一个项目
  */
+@RestController
 @SpringBootApplication
 public class MylearnApplication {
 
@@ -13,4 +21,18 @@ public class MylearnApplication {
         SpringApplication.run(MylearnApplication.class, args);
     }
 
+    @GetMapping("/demo")
+    public String demo() {
+        return "hello world";
+    }
+
+    @Bean
+    public CommandLineRunner CommandLineRunner(ApplicationContext applicationContext) {
+        return args -> {
+            System.out.println("spring默认提供的Bean------");
+            String[] names = applicationContext.getBeanDefinitionNames();
+            Arrays.sort(names);
+            Arrays.stream(names).forEach(System.out::println);
+        };
+    }
 }
