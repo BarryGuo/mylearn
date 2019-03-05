23afcdf021005cf7ef33b4acf36019558943d2b5
10122677c <guozhongpeng@boe.com.cn>
2019-03-05 14:19:36 +0800

    纪念我的第一个项目~

--- src/main/java/com/peng/mylearn/MylearnApplication.java
+++ src/main/java/com/peng/mylearn/MylearnApplication.java
@@ -3,6 +3,9 @@ package com.peng.mylearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 纪念我的第一个项目
+ */
 @SpringBootApplication
 public class MylearnApplication {
 
